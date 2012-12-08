e485b4ec6094fcd74e84d403f4f25ea5f62bed67
Tommy Chen <tommy351@gmail.com>
2012-12-08 17:32:14 +0800

    WordPress migrator released

---
+++ migrator/wordpress/README.md
@@ -0,0 +1,47 @@
+# WordPress migrator plugin for [Hexo]
+
+This plugin can migrate your blog from WordPress to Hexo.
+
+## Usage
+
+### Install
+
+```
+npm install hexo-migrator-wordpress --save
+```
+
+### Enable
+
+Add `hexo-migrator-wordpress` to `plugins` in `_config.yml`.
+
+``` yaml
+plugins:
+- hexo-migrator-wordpress
+```
+
+### Disable
+
+Remove `hexo-migrator-wordpress` from `plugins` in `_config.yml`.
+
+``` yaml
+plugins:
+- hexo-migrator-wordpress
+```
+
+### Update
+
+Execute the following command.
+
+```
+npm update
+```
+
+### Uninstall
+
+Execute the following command. Don't forget to disable the plugin before uninstalling.
+
+```
+npm uninstall hexo-migrator-wordpress
+```
+
+[Hexo]: http://zespia.tw/hexo
---
+++ migrator/wordpress/index.js
@@ -0,0 +1,109 @@
+var extend = hexo.extend,
+  util = hexo.util,
+  file = util.file,
+  sourceDir = hexo.source_dir,
+  xml2js = require('xml2js'),
+  parser = new xml2js.Parser(),
+  request = require('request'),
+  async = require('async'),
+  _ = require('underscore'),
+  tomd = require('to-markdown').toMarkdown;
+
+extend.migrator.register('wordpress', function(args){
+  var source = args.shift();
+
+  if (!source) return console.log('\nUsage: hexo migrate wordpress <source>\n\nMore info: http://zespia.tw/hexo/docs/migrate.html\n');
+
+  async.waterfall([
+    function(next){
+      console.log('Fetching %s.', source);
+
+      // URL regular expression from: http://blog.mattheworiordan.com/post/13174566389/url-regular-expression-for-links-with-or-without-the
+      if (source.match(/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[.\!\/\\w]*))?)/)){
+        request(source, function(err, res, body){
+          if (err) throw err;
+          if (res.statusCode == 200) next(null, body);
+        });
+      } else {
+        file.read(source, next);
+      }
+    },
+    function(data, next){
+      console.log('Parsing XML.');
+      parser.parseString(data, next);
+    },
+    function(data, next){
+      console.log('Analyzing.');
+
+      var length = 0,
+        arr = data.rss.channel[0].item;
+
+      async.forEach(arr, function(item, next){
+        var postTitle = item.title[0],
+          postDate = item['wp:post_date'][0],
+          postLink = item['wp:post_name'][0],
+          postContent = item['content:encoded'][0],
+          postComment = item['wp:comment_status'][0] === 'open' ? true : false;
+
+        if (!postLink || _.isObject(postLink)) postLink = postTitle.toLowerCase().split(' ').join('-');
+        if (_.isObject(postTitle)) postTitle = '';
+
+        postContent = _.isObject(postContent) ? '' : tomd(postContent);
+
+        switch (item['wp:post_type'][0]){
+          case 'post':
+            length++;
+
+            var postStatus = item['wp:status'][0] === 'publish' ? '_posts/' : '_drafts/',
+              cats = item.category,
+              postTag = [];
+
+            _.each(cats, function(item){
+              if (!_.isString(item) && item.$.domain === 'tag'){
+                postTag.push(item._);
+              }
+            });
+
+            if (postTag.length) postTag = '\n- ' + _.uniq(postTag).join('\n- ');
+
+            var content = [
+              '---',
+              'layout: post',
+              'title: ' + postTitle,
+              'date: ' + postDate,
+              'comments: ' + postComment,
+              'tags: ' + (postTag ? postTag : ''),
+              '---'
+            ];
+
+            file.write(sourceDir + postStatus + postLink + '.md', content.join('\n') + '\n\n' + postContent, next);
+            break;
+
+          case 'page':
+            length++;
+
+            var content = [
+              '---',
+              'layout: page',
+              'title: ' + postTitle,
+              'date: ' + postDate,
+              'comments: ' + postComment,
+              '---'
+            ];
+
+            file.write(sourceDir + postLink + '/index.md', content.join('\n') + '\n\n' + postContent, next);
+            break;
+
+          default:
+            next();
+        }
+      }, function(err){
+        if (err) throw err;
+        next(null, length);
+      });
+    }
+  ], function(err, length){
+    if (err) throw err;
+    console.log('%d posts migrated.', length);
+  });
+});
