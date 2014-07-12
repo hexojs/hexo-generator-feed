8bca4a8a8dcdc463fb032dd34c53927b1b07e884
Tommy Chen <tommy351@gmail.com>
2014-07-13 00:21:44 +0800

    Add generator name

--- index.js
+++ index.js
@@ -1,54 +1,7 @@
-var ejs = require('ejs'),
-  merge = require('utils-merge'),
-  path = require('path'),
-  file = hexo.util.file2;
+var generator = hexo.extend.generator;
 
-ejs.filters.cdata = function(str){
-  return '<![CDATA[' + (str || '') + ']]>';
-};
-
-var atomTmplSrc = path.join(__dirname, 'atom.ejs'),
-  atomTmpl = ejs.compile(file.readFileSync(atomTmplSrc));
-
-var rss2TmplSrc = path.join(__dirname, 'rss2.ejs'),
-  rss2Tmpl = ejs.compile(file.readFileSync(rss2TmplSrc));
-
-hexo.extend.generator.register(function(locals, render, callback){
-  var config = hexo.config;
-
-  var feedConfig = merge({
-    type: 'atom',
-    limit: 20
-  }, config.feed);
-
-  // Restrict feed type
-  if (feedConfig.type !== 'atom' && feedConfig.type !== 'rss2'){
-    feedConfig.type = 'atom';
-  }
-
-  // Set default feed path
-  if (!feedConfig.path){
-    feedConfig.path = feedConfig.type + '.xml';
-  }
-
-  // Add extension name if don't have
-  if (!path.extname(feedConfig.path)){
-    feedConfig.path += '.xml';
-  }
-
-  // Determine which template to use
-  if (feedConfig.type === 'rss2'){
-    var template = rss2Tmpl;
-  } else {
-    var template = atomTmpl;
-  }
-
-  var xml = template({
-    config: config,
-    posts: locals.posts.sort('date', -1).limit(feedConfig.limit),
-    feed_url: config.root + feedConfig.path
-  });
-
-  hexo.route.set(feedConfig.path, xml);
-  callback();
-});
+if (generator.register.length === 1){
+  generator.register(require('./feed'));
+} else {
+  generator.register('feed', require('./feed'));
+}
