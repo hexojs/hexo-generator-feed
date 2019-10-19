5d7f646f813b9bd328866655eefea8c9f900906a
curbengh <43627182+curbengh@users.noreply.github.com>
2019-10-19 08:24:17 +0100

    fix(config): handle invalid configs robustly

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 /* global hexo */
 'use strict';
 
+const { extname } = require('path');
+
 const config = hexo.config.feed = Object.assign({
   type: 'atom',
   limit: 20,
@@ -13,6 +15,7 @@ const config = hexo.config.feed = Object.assign({
 }, hexo.config.feed);
 
 let type = config.type;
+let path = config.path;
 const feedFn = require('./lib/generator');
 
 if (typeof type === 'string') {
@@ -23,20 +26,47 @@ if (typeof type === 'string') {
     type = 'atom';
   }
 
+  if (!path) path = type.concat('.xml');
+
+  // Add extension name if not found
+  if (!extname(path)) {
+    path += '.xml';
+  }
+
   config.type = type;
+  config.path = path;
 
   hexo.extend.generator.register('feed', function(locals) {
-    return feedFn.call(hexo, locals, type);
+    return feedFn.call(hexo, locals);
   });
 } else {
   type = type.map(str => str.toLowerCase());
 
   if (type.length === 1) {
     if (type[0] === 'atom') type.push('rss2');
-    else type.push('atom');
+    else if (type[0] === 'rss2') type.push('atom');
+    else {
+      type[0] = 'atom';
+      type.push('rss2');
+    }
   }
 
+  if (!path) path = type.map(str => str.concat('.xml'));
+
+  if (typeof path === 'string') path = [path];
+
+  if (path.length === 1) {
+    if (path[0].toLowerCase().includes('atom')) path.push('rss2.xml');
+    else path.push('atom.xml');
+  }
+
+  path = path.map(str => {
+    if (!extname(str)) return str.concat('.xml');
+    return str;
+  });
+
   config.type = type;
+  config.path = path;
 
   hexo.extend.generator.register('feed1', function(locals) {
     return feedFn.call(hexo, locals, type[0]);
